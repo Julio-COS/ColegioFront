bab20d7154b2be7b53b520c9fc8559b68f9a689a
Julio-COS <slzsalvador98@gmail.com>
2024-09-15 21:35:16 -0500

    add interface familiar

---
+++ src/app/interface/RelacionApoderado.ts
@@ -0,0 +1,31 @@
+export interface RelacionApoderado{
+    idRelacionApoderado:    number;
+    idEstudiante:           number;
+    idApoderado:            number;
+    tipoRelacion:           string;
+}
+
+
+export class RelacionApoderadoResponse{
+    idRelacionApoderado=0;
+    idEstudiante=0;
+    idApoderado=0;
+    tipoRelacion='';
+}
+
+export interface RelacionApoderadoInfo{
+    idRelacionApoderado:    number;
+    relacionApoderado:      string;
+    nombreEstudiante:       string;
+    nombreApoderado:        string;
+    tipoRelacion:           string;
+}
+
+
+export class RelacionApoderadoInfoResponse{
+    idRelacionApoderado=0;
+    relacionApoderado='';
+    nombreEstudiante='';
+    nombreApoderado='';
+    tipoRelacion='';
+}
